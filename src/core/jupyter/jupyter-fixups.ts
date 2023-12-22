0b1f6d03defcc1b884a1cedb9b96bb40218abee7
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-12-22 15:27:47 +0100

    Improve title snipping in notebooks
    Don't perform notebook title fixup with `format: revealjs` as this would create a new undesired title slide.
    
    Follow up on 24672a4affb8d3c881be9a3b9ede641d2e26d9d9

--- src/core/jupyter/jupyter-fixups.ts
+++ src/core/jupyter/jupyter-fixups.ts
@@ -167,7 +167,7 @@ export function fixupFrontMatter(nb: JupyterNotebook): JupyterNotebook {
 
   // if we have front matter and a title then we are done
   const yaml = partitioned ? readYamlFromMarkdown(partitioned.yaml) : undefined;
-  if (yaml?.title) {
+  if (yaml?.title || yaml?.format?.["revealjs"]) {
     return nb;
   }
 
